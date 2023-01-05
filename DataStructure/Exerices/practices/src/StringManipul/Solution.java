83e8b39041aa6cd60694b3e658fe056c89cd01ba
gloriadukuzeyesu <gloriadukuzeyesu@gmail.com>
2023-01-04 19:04:49 -0700

    new notes

--- DataStructure/Exerices/practices/src/StringManipul/Solution.java
+++ DataStructure/Exerices/practices/src/StringManipul/Solution.java
@@ -1,5 +1,8 @@
 package StringManipul;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
     /**
      * @param word a string to check if it is a valid palindrome
@@ -33,10 +36,40 @@ public class Solution {
        return true;
     }
 
-    public static void main(String[] args) {
-        var resp = isValidPalindrome("A man, a plan, a canal: Panama.");
-        var resp2 = isValidPalindrome("leveL");
-        System.out.println(resp + " " + resp2);
+    public static boolean tripletSum(int [] array, int sum) {
+        if( array.length < 3) {
+            return false;
+        }
 
+        Set<Integer> set = new HashSet<>();
+
+        int index = 0;
+        while ( index < array.length) {
+            int first = array[index];
+            for (int currentValue : array) {
+                int temp = sum - (currentValue + first);
+                if (set.contains(temp)) {
+                    System.out.println(first + " " + temp + " " + currentValue);
+                    return true;
+                }
+                else{
+                    set.add(currentValue);
+                }
+
+            }
+            index++;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+//        var resp = isValidPalindrome("A man, a plan, a canal: Panama.");
+//        var resp2 = isValidPalindrome("leveL");
+//        System.out.println(resp + " " + resp2);
+
+        int[] arr = new int[] {1,2,6};
+        int sum = 9;
+        var response = tripletSum(arr, sum);
+        System.out.println(response);
     }
 }
